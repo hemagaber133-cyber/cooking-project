ee90ecaadad9e25c5e6af067818c6f123e0b23fe
‪Hema Gaber‬‏ <hemagaber133@gmail.com>
2026-05-03 02:23:55 +0300

    complete css for home and JS for theme btn

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-// دا علشان المنيو تظهر و تختفي لما تضغط علي زر المنيو
+//---------------- Hema Edit ------------------
 const menu= document.querySelector('.menu-btn');
 const links= document.querySelector('.links');
 menu.addEventListener('click',()=>{ 
     links.classList.toggle('hide');
 })
+function ChangeTheme() {
+    const themeLink = document.getElementById('theme-link');
+    const currentTheme = themeLink.getAttribute('href');
+
+    if (currentTheme.includes('themegirls.css')) {
+        themeLink.setAttribute('href', 'themeboys.css');
+        localStorage.setItem('selectedTheme', 'boys'); 
+    } else {
+        themeLink.setAttribute('href', 'themegirls.css');
+        localStorage.setItem('selectedTheme', 'girls');
+    }
+}
+window.onload = function() {
+    const savedTheme = localStorage.getItem('selectedTheme');
+    const themeLink = document.getElementById('theme-link');
+
+    if (savedTheme === 'boys') {
+        themeLink.setAttribute('href', 'themeboys.css');
+    } else if (savedTheme === 'girls') {
+        themeLink.setAttribute('href', 'themegirls.css');
+    }
+};
